47bcd6d7778f8e66beaa3b01d6e3b4fa784e4f31
Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
2015-06-29 04:00:49 +0300

    emu: make it dump the DF file name
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>

--- emu/emu_main.c
+++ emu/emu_main.c
@@ -5,6 +5,7 @@
 #include "emu_syntax.h"
 #include "emu_ast.h"
 #include "emu_glue.h"
+#include "dump.h"
 
 #include <stdio.h>
 #include <stdbool.h>
@@ -32,6 +33,19 @@ int main(int argc, char **argv)
 
 	if (had_errors)
 		return 3;
+
+	const struct emu_property *prop = df_get_property(df, "name");
+	if (!prop)
+		return 6;
+
+	const struct emu_value *value = property_get_value(prop, 1);
+	if (!value)
+		return 7;
+
+	size_t buf_len;
+	const unsigned char *buf = value_get(value, &buf_len);
+
+	dump_buffer(buf, buf_len, stdout);
 
 	df_free(df);
 
