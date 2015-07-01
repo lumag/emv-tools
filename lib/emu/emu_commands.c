bc2dc821808db8dcc3c16b621d5cc7c54fb07a46
Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
2015-07-01 07:06:51 +0300

    emu: add simple command execution engine
    Execute simple commands that just retrieve data from DF.
    
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>

---
+++ lib/emu/emu_commands.c
@@ -0,0 +1,159 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "openemv/emu_ast.h"
+#include "openemv/emu_glue.h"
+
+#include <stdint.h>
+
+static uint16_t emu_error(struct emu_card *card, const unsigned char **ret, size_t *ret_len, uint16_t sw)
+{
+	*ret = NULL;
+	*ret_len = 0;
+
+	return sw;
+}
+
+static uint16_t emu_command_ins_not_supported(struct emu_card *card, uint8_t p1, uint8_t p2, const unsigned char **ret, size_t *ret_len)
+{
+	return emu_error(card, ret, ret_len, 0x6d00);
+}
+
+static uint16_t emu_command_select(struct emu_card *card, uint8_t p1, uint8_t p2, const unsigned char **ret, size_t *ret_len)
+{
+	const struct emu_df *df;
+
+	if (p1 != 4 || p2 != 0)
+		return emu_error(card, ret, ret_len, 0x6a86);
+
+	df = card_get_df(card, NULL, 0); // FIXME
+	if (!df)
+		return emu_error(card, ret, ret_len, 0x6a82);
+
+	*ret = df_get_value(df, "fci", 1, ret_len);
+	if (!ret)
+		return emu_error(card, ret, ret_len, 0x6a80);
+
+	return 0x9000;
+}
+
+static uint16_t emu_command_read_record(struct emu_card *card, uint8_t p1, uint8_t p2, const unsigned char **ret, size_t *ret_len)
+{
+	const struct emu_df *df;
+	char tag[6];
+
+	if ((p2 & 0x7) != 4)
+		return emu_error(card, ret, ret_len, 0x6a86);
+
+	df = card_get_df(card, NULL, 0); // FIXME
+	if (!df)
+		return emu_error(card, ret, ret_len, 0x6a82);
+
+	snprintf(tag, sizeof(tag), "sfi%d", p2 >> 3);
+
+	*ret = df_get_value(df, tag, p1, ret_len);
+	if (!*ret)
+		// FIXME -- differentiate between record not present and hidden record
+		return emu_error(card, ret, ret_len, 0x6a80);
+
+	return 0x9000;
+}
+
+static uint16_t emu_command_emv_generate_ac(struct emu_card *card, uint8_t p1, uint8_t p2, const unsigned char **ret, size_t *ret_len)
+{
+	const struct emu_df *df;
+
+	if (p2 != 0)
+		return emu_error(card, ret, ret_len, 0x6a86);
+
+	df = card_get_df(card, NULL, 0); // FIXME
+	if (!df)
+		return emu_error(card, ret, ret_len, 0x6a82);
+
+	*ret = df_get_value(df, "ac", 1, ret_len);
+	if (!ret)
+		return emu_error(card, ret, ret_len, 0x6a80);
+
+	return 0x9000;
+}
+
+static uint16_t emu_command_emv_get_processing_options(struct emu_card *card, uint8_t p1, uint8_t p2, const unsigned char **ret, size_t *ret_len)
+{
+	const struct emu_df *df;
+
+	if (p1 != 0 || p2 != 0)
+		return emu_error(card, ret, ret_len, 0x6a86);
+
+	df = card_get_df(card, NULL, 0); // FIXME
+	if (!df)
+		return emu_error(card, ret, ret_len, 0x6a82);
+
+	*ret = df_get_value(df, "gpo", 1, ret_len);
+	if (!ret)
+		return emu_error(card, ret, ret_len, 0x6a80);
+
+	return 0x9000;
+}
+
+static uint16_t emu_command_emv_get_data(struct emu_card *card, uint8_t p1, uint8_t p2, const unsigned char **ret, size_t *ret_len)
+{
+	const struct emu_df *df;
+	char tag[9];
+
+	df = card_get_df(card, NULL, 0); // FIXME
+	if (!df)
+		return emu_error(card, ret, ret_len, 0x6a82);
+
+	snprintf(tag, sizeof(tag), "data%02x%02x", p1, p2);
+
+	*ret = df_get_value(df, tag, 1, ret_len);
+	if (!*ret)
+		return emu_error(card, ret, ret_len, 0x6a88);
+
+	return 0x9000;
+}
+
+
+static uint16_t emu_command_cla_00(struct emu_card *card, uint8_t ins, uint8_t p1, uint8_t p2, const unsigned char **ret, size_t *ret_len)
+{
+	switch (ins) {
+	case 0xa4:
+		return emu_command_select(card, p1, p2, ret, ret_len);
+	case 0xb2:
+		return emu_command_read_record(card, p1, p2, ret, ret_len);
+	default:
+		return emu_command_ins_not_supported(card, p1, p2, ret, ret_len);
+	}
+}
+
+static uint16_t emu_command_cla_80(struct emu_card *card, uint8_t ins, uint8_t p1, uint8_t p2, const unsigned char **ret, size_t *ret_len)
+{
+	switch (ins) {
+	case 0xa8:
+		return emu_command_emv_get_processing_options(card, p1, p2, ret, ret_len);
+	case 0xae:
+		return emu_command_emv_generate_ac(card, p1, p2, ret, ret_len);
+	case 0xca:
+		return emu_command_emv_get_data(card, p1, p2, ret, ret_len);
+	default:
+		return emu_command_ins_not_supported(card, p1, p2, ret, ret_len);
+	}
+}
+
+static uint16_t emu_command_cla_not_supported(struct emu_card *card, uint8_t ins, uint8_t p1, uint8_t p2, const unsigned char **ret, size_t *ret_len)
+{
+	return emu_error(card, ret, ret_len, 0x6e00);
+}
+
+uint16_t emu_command(struct emu_card *card, uint8_t cla, uint8_t ins, uint8_t p1, uint8_t p2, const unsigned char **ret, size_t *ret_len)
+{
+	switch (cla) {
+	case 0x00:
+		return emu_command_cla_00(card, ins, p1, p2, ret, ret_len);
+	case 0x80:
+		return emu_command_cla_80(card, ins, p1, p2, ret, ret_len);
+	default:
+		return emu_command_cla_not_supported(card, ins, p1, p2, ret, ret_len);
+	}
+}
--- lib/include/openemv/emu_glue.h
+++ lib/include/openemv/emu_glue.h
@@ -7,5 +7,6 @@ struct emu_card;
 struct emu_card *card_parse(const char *fname);
 void card_free(struct emu_card *card);
 const struct emu_df *card_get_df(const struct emu_card *card, const unsigned char *name, size_t len);
+uint16_t emu_command(struct emu_card *card, uint8_t cla, uint8_t ins, uint8_t p1, uint8_t p2, const unsigned char **data, size_t *data_len);
 
 #endif
