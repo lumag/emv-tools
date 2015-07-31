ca648ddcb676ddfbf4fb9af5b0627a2908ee29f7
Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
2015-07-31 19:36:29 +0300

    Add simple library configuration module
    Add a simple module returning strings for requested "path" inside a
    configuration file. Internally the libconfig is used to handle config
    files.
    
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>

---
+++ lib/config.c
@@ -0,0 +1,49 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "openemv/config.h"
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <libconfig.h>
+
+static config_t *_openemv_config;
+
+static void openemv_init_config(void)
+{
+	config_t *config;
+	int ret;
+	const char *fname;
+
+	config = malloc(sizeof(*_openemv_config));
+	config_init(config);
+	fname = getenv("OPENEMV_CONFIG");
+	if (!fname)
+		fname = OPENEMV_CONFIG_DIR "config.txt";
+	ret = config_read_file(config, fname);
+	if (ret != CONFIG_TRUE) {
+		config_destroy(config);
+		free(config);
+	} else
+		_openemv_config = config;
+}
+
+const char *openemv_config_get(const char *path)
+{
+	const char *value;
+	int ret;
+
+	if (!_openemv_config)
+		openemv_init_config();
+
+	if (!_openemv_config)
+		return NULL;
+
+	ret = config_lookup_string(_openemv_config, path, &value);
+
+	if (ret != CONFIG_TRUE)
+		return NULL;
+
+	return value;
+}
---
+++ lib/include/openemv/config.h
@@ -0,0 +1,6 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const char *openemv_config_get(const char *path);
+
+#endif
