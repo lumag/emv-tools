17ed43b1008c5ab02d16c206ef0b6e6fae32b814
Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
2015-08-26 12:07:17 +0300

    crypto: nettle: hack up an internal RNG
    THis is required for genkey support. Please, do not use it yet.
    
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>

--- lib/crypto/crypto_nettle.c
+++ lib/crypto/crypto_nettle.c
@@ -27,6 +27,7 @@
 #include <nettle/sha.h>
 #include <nettle/bignum.h>
 #include <nettle/rsa.h>
+#include <nettle/yarrow.h>
 
 struct crypto_hash_nettle {
 	struct crypto_hash ch;
@@ -146,6 +147,100 @@ static struct crypto_pk *crypto_pk_nettle_open(enum crypto_algo_pk pk, va_list v
 	return &cp->cp;
 }
 
+#define RND_N_SOURCES 2
+
+static struct {
+	struct yarrow256_ctx yactx;
+	struct yarrow_source sources[RND_N_SOURCES];
+} rndctx;
+
+#if defined(HAVE_GETENTROPY)
+#include <unistd.h>
+/* getentropy call is declared in unistd.h */
+#elif defined(linux)
+#include <linux/random.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+
+#if defined SYS_getrandom
+static int getrandom(void *buf, size_t buflen, unsigned int flags)
+{
+	return syscall(SYS_getrandom, buf, buflen, flags);
+}
+#else
+#error Please provide getrandom emulation
+#endif
+
+static int getentropy(void *buf, size_t buflen)
+{
+	int ret;
+
+	if (buflen > 256)
+		goto failure;
+
+	ret = getrandom(buf, buflen, 0);
+	if (ret < 0)
+		return ret;
+	if (ret == buflen)
+		return 0;
+failure:
+	errno = EIO;
+	return -1;
+}
+#else
+#error Your system is not yet supported. Please add a getentropy function
+#endif
+
+#define GETENTROPY_BUF_SIZE 16
+
+static int rnd_source_getentropy(int init)
+{
+	unsigned char buf[GETENTROPY_BUF_SIZE];
+	unsigned int read_size = sizeof(buf);
+	int rc;
+
+	rc = getentropy(buf, read_size);
+	if (rc < 0)
+		return rc;
+
+	return yarrow256_update(&rndctx.yactx, init,
+			read_size * 8 / 2, read_size, buf);
+
+}
+
+static void rnd_func(void *_ctx, size_t length, uint8_t *data)
+{
+	yarrow256_random(&rndctx.yactx, length, data);
+}
+
+static bool rnd_reseed(void)
+{
+	if (rnd_source_getentropy(0) < 0)
+		return false;
+
+	if (rnd_source_getentropy(1) < 0)
+		return false;
+
+	yarrow256_slow_reseed(&rndctx.yactx);
+
+	return true;
+}
+
+static bool rnd_init(void)
+{
+	static bool initialized = false;
+
+	if (initialized)
+		return true;
+
+	yarrow256_init(&rndctx.yactx, RND_N_SOURCES, rndctx.sources);
+
+	initialized = rnd_reseed();
+
+	return initialized;
+}
+
 static struct crypto_backend crypto_nettle_backend = {
 	.hash_open = crypto_hash_nettle_open,
 	.pk_open = crypto_pk_nettle_open,
@@ -153,5 +248,7 @@ static struct crypto_backend crypto_nettle_backend = {
 
 struct crypto_backend *crypto_nettle_init(void)
 {
+	if (!rnd_init())
+		return NULL;
 	return &crypto_nettle_backend;
 }
