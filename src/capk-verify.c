1d9b0a9f9a4bf465bee7a3efa789c55a8474c182
Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
2015-11-06 03:42:07 +0300

    capk-verify: default to reading capk.txt in cwd
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>

--- src/capk-verify.c
+++ src/capk-verify.c
@@ -33,7 +33,7 @@ int main(int argc, char **argv) {
 	const char *fname;
 	int rc = 0;
 
-	fname = openemv_config_get("capk");
+	fname = openemv_config_get_def("capk", "capk.txt");
 
 	f = fopen(fname, "r");
 	if (!f) {
