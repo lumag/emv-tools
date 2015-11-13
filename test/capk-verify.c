145bb69af0c45f8bc48b0210757b51bcb2ce6af7
Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
2015-11-14 02:22:51 +0300

    capk-verify: support symlinking key files
    Add an option to capk-verify that creates RID_IDX.0 symlinks pointing to
    corresponding key file. This is a preparation to adding custom CA PK
    keys.
    
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>

--- test/capk-verify.c
+++ test/capk-verify.c
@@ -23,19 +23,38 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
 
 #ifndef BUFSIZ
 #define BUFSIZ 4096
 #endif
 
-int main(int argc, char **argv) {
-	FILE *f;
-	const char *fname;
+static void do_pk_symlink(const char *fname, const char *linkdir, const struct emv_pk *pk)
+{
+	char *linkname = emv_pk_get_ca_pk_file(linkdir, pk->rid, pk->index);
+	if (!linkname)
+		return;
+
+	unlink(linkname);
+	int ret = symlink(fname, linkname);
+	if  (ret < 0)
+		perror("symlink");
+
+	free(linkname);
+}
+
+static int do_one_file(const char *fname, const char *linkdir, bool dump)
+{
 	int rc = 0;
+	FILE * f;
 
-	fname = openemv_config_get_str("capk.file", "capk.txt");
-
-	f = fopen(fname, "r");
+	if (!strcmp(fname, "-")) {
+		f = stdin;
+		linkdir = NULL; /* Do not symlink stdin */
+	} else
+		f = fopen(fname, "r");
 	if (!f) {
 		perror("fopen");
 		return 1;
@@ -46,8 +65,12 @@ int main(int argc, char **argv) {
 		if (fgets(buf, sizeof(buf), f) == NULL)
 			break;
 		struct emv_pk *pk = emv_pk_parse_pk(buf);
-		if (!pk)
-			continue;
+		if (!pk) {
+			fprintf(stderr, "Can't parse public key\n");
+			rc = 1;
+			break;
+		}
+
 		fprintf(stderr, "Verifying CA PK for %02hhx:%02hhx:%02hhx:%02hhx:%02hhx IDX %02hhx %zd bits...",
 				pk->rid[0],
 				pk->rid[1],
@@ -58,12 +81,14 @@ int main(int argc, char **argv) {
 				pk->mlen * 8);
 		if (emv_pk_verify(pk)) {
 			fprintf(stderr, "OK\n");
-			if (argc > 2 && argv[2][0] == 'v') {
+			if (dump) {
 				char *c = emv_pk_dump_pk(pk);
-				if (c)
-					printf("%s\n", c);
+				printf("%s\n", c);
 				free(c);
 			}
+
+			if (linkdir)
+				do_pk_symlink(fname, linkdir, pk);
 		} else {
 			fprintf(stderr, "Failed!\n");
 			rc = 1;
@@ -71,7 +96,48 @@ int main(int argc, char **argv) {
 		emv_pk_free(pk);
 	}
 
-	fclose(f);
+	/* Don't close stdin */
+	if (strcmp(fname, "-"))
+		fclose(f);
+
+	return rc;
+}
+
+static int usage(const char *progname)
+{
+	fprintf(stderr, "Usage: %s [-d] [-l dir] capk...\n", progname);
+
+	return 1;
+}
+
+int main(int argc, char **argv) {
+	int rc = 0;
+	bool dump = false;
+	const char *linkdir = NULL;
+	const char *progname;
+
+	progname = *(argv++);
+
+	while (*argv) {
+		const char *opt = *(argv++);
+
+		if (!strcmp(opt, "-d"))
+			dump = true;
+		else if (!strcmp(opt, "-l") && argv[0])
+			linkdir = *(argv++);
+		else if (!strcmp(opt, "-") || opt[0] != '-') {
+			argv--;
+			break;
+		} else
+			return usage(progname);
+	}
+
+	if (!*argv)
+		return do_one_file(openemv_config_get_str("capk.file", "capk.txt"), linkdir, dump);
+
+	while (*argv && !rc) {
+		rc = do_one_file(*(argv++), linkdir, dump);
+	}
 
 	return rc;
 }
